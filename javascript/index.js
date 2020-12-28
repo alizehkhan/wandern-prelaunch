c6d61b599b842879b243c40d8940e2a119ec34b4
Seyhan Van Khan <44237992+seyhanvankhan@users.noreply.github.com>
2020-12-28 00:34:09 +0000

    Temporary validation messages

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,6 @@
 function addEmailToAirtable(theForm) {
+  document.getElementById("email").style.cursor = "progress";
+
   const data = {"Email": theForm.email.value};
   fetch("https://36077.wayscript.io/", {
     method: "POST",
@@ -6,10 +8,12 @@ function addEmailToAirtable(theForm) {
     mode: "cors",
     body: JSON.stringify(data),
   }).then(resp => {
-    if (resp.status === 500) {
-      console.log("Oops! Invalid Email")
+    if (resp.status === 200) {
+      console.log("You're in!")
     } else {
-      console.log("You're in! Valid Email")
+      console.log("Oops! Something went wrong")
     }
+    document.getElementById("email").value = "";
+    document.getElementById("email").style.cursor = "";
   });
 }
